748cbc5d8240af120261188f7db441d232d1a104
iby1812 <33149774+iby1812@users.noreply.github.com>
2018-05-31 13:10:45 +0300

    Add files via upload

--- Board.cpp
+++ Board.cpp
@@ -2,6 +2,7 @@
 
 //Constructor Functions -
 
+
 Board::Board(int value)
 {
 	this->n = value;
@@ -15,6 +16,9 @@ Board::Board(int value)
 
 Board::Board(const Board& other)
 {
+	/*if(size()>0){
+		this->~Board();
+	}*/
 	this->n = other.size();
 	mat = new Node*[n];
 
@@ -100,7 +104,7 @@ Board& Board::operator=(char value)
 Board& Board::operator=(const Board& db)
 {
 	this->~Board();
-	
+
 	this->n = db.n;
 	mat = new Node*[n];
 
@@ -117,4 +121,70 @@ Board& Board::operator=(const Board& db)
 		}
 	}
 	return *this;
+}
+Board& Board::operator=(int value){
+
+
+	n = value;
+	mat = new Node*[n];
+
+	for(int i = 0; i < n ;i++)
+	{
+		mat[i] = new Node[n];
+	}
+	return *this;
+
+}
+struct RGB {
+	uint8_t red, green, blue;
+public:
+	RGB() {}
+	RGB(uint8_t red, uint8_t green, uint8_t blue): red(red), green(green), blue(blue) {}
+};
+string Board::draw(int val){
+	const int dimx = val, dimy = val;
+	string filename="cpp.ppm";
+	ofstream imageFile(filename, ios::out | ios::binary);
+	imageFile << "P6" << endl << dimx <<" " << dimy << endl << 255 << endl;
+	RGB image[dimx*dimy];
+	int charsize=val/n;
+	int count=1;
+
+	for (int j = 0; j < n; ++j)  {  // row
+		for (int i = 0; i < n; ++i) { // column
+
+			if(mat[j][i].getSymbol()=='X')
+			{
+				for(int f=count*1;f<=count*charsize;f++){
+					for(int g=count*1;g<=count*charsize;g++)
+					{
+						image[f+g].red = (255);
+						image[f+g].green = (255);
+						image[f+g].blue = ( (255));
+					}
+				}
+			}
+			else if (mat[j][i].getSymbol()=='O'){
+				for(int f=count*1;f<=count*charsize;f++){
+					for(int g=count*1;g<=count*charsize;g++){
+						image[dimx*j+i].red = (0);
+						image[dimx*j+i].green = (0);
+						image[dimx*j+i].blue = ( (0));
+					}
+				}
+				count++;
+			}
+
+
+		}
+	}
+	image[0].red = 255;
+	image[0].blue = 0;
+	image[0].green = 0;
+	///
+	///image processing
+	///
+	imageFile.write(reinterpret_cast<char*>(&image), 3*dimx*dimy);
+	imageFile.close();
+	return filename;
 }
--- Board.h
+++ Board.h
@@ -1,5 +1,6 @@
 #pragma once
-#include <iostream>
+#include<iostream>
+#include<fstream>
 #include <list>
 #include <string>
 
@@ -14,10 +15,13 @@ using namespace std;
 class Board{
 
 public:
-    
+
 	Node **mat;
 	int n;
-
+	Board(){
+		n=0;
+		mat=NULL;
+	}
 	Board(int value);
 	Board(const Board& other);
 	~Board();
@@ -28,11 +32,44 @@ public:
 	Node& operator[](const BoardIndex& coor) const;
 	Board& operator=(char value);
 	Board& operator=(const Board& db);
-
+	Board& operator=(int value);
 	friend ostream& operator<<(ostream& os, Board const &b);
+	friend istream& operator>>(istream& is, Board  &b);
 	friend bool operator==(const Board& b1, const Board& b2);
+	string draw(int val);
 
-};
+
+	};
+
+inline istream& operator>>(istream& is, Board &b){
+	string f;
+		is>>f;
+		ifstream file;
+		file.open(f);
+		bool once=true;
+		int i=0;
+		string line;
+	if(file.fail()){
+		cout<<"fail";
+	}
+	else{
+		while(file>>line)
+		{
+			if(once)
+			{
+				b=(int)line.length();
+				once=false;
+			}
+			for(unsigned int j=0;j<line.length();j++)
+			{
+				b[{i,(int)j}]=line.at((int)j);
+			}
+			i++;
+		}
+	}
+		file.close();
+		return is;
+}
 
 inline ostream& operator<<(ostream& os, Board const &b) {
 
@@ -62,7 +99,7 @@ inline bool operator==(const Board& b1, const Board& b2){
 				if(b1.mat[i][j].getSymbol() != b2.mat[i][j].getSymbol()){
 					return false;
 				}
-				
+
 			}
 		}
 		return true;
